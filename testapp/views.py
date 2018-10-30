89fb74a7659c7a5dfd2acdd0bbc923b4c1f159a0
HirokiIshii <h_ishii@asobimo.com>
2018-10-30 10:53:21 +0900

    Viewの引数 Viewのテンプレート（htmlによるページデザイン）

--- testapp/views.py
+++ testapp/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
 
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('testapp/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    #return HttpResponse(output)
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 
 # Create your views here.
