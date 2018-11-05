512daa4ac758dc176e447f63d77782be7b3a0893
HirokiIshii <h_ishii@asobimo.com>
2018-11-05 14:27:32 +0900

    ビューを汎用ビューに書き替え

--- testapp/urls.py
+++ testapp/urls.py
@@ -4,9 +4,15 @@ from . import views
 
 app_name = 'testapp'    #アプリの名前空間定義。templateからURLを正確に判断するためのもの
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
 
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    #path('<int:question_id>/', views.detail, name='detail'),
+    #path('<int:question_id>/results/', views.results, name='results'),
+#------------------------------------------------------------------------------------------------
+# 以下は汎用ビューを使った書き方
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 from django.db.models import F
 
 
@@ -35,6 +36,24 @@ def detail(request, question_id):                                           #get
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'testapp/results.html', {'question': question})
+
+#---------------------------------------------------------------------------------------------------------------------------------------
+# 以上は簡単だが冗長な書き方。以下は汎用ビューを使った書き方
+class IndexView(generic.ListView):
+    template_name = 'testapp/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'testapp/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'testapp/results.html'
 
 def vote(request, question_id):
     # Questionオブジェクトを取得
