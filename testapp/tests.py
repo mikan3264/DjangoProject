c626870427e56e7e73a9fa0da5e23858ac321e56
HirokiIshii <h_ishii@asobimo.com>
2018-11-05 15:55:07 +0900

    テストクラス追加

--- testapp/tests.py
+++ testapp/tests.py
@@ -30,3 +30,14 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
