fbffe8be18c3f6eb4db5b9e42e40be26406e82a6
mikan3264 <ishii.hiro.80213235@gmail.com>
2019-03-23 12:02:44 +0900

    mysqlパスワード変更

--- DjangoWebProject3/settings.py
+++ DjangoWebProject3/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_test',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'porfavor',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
