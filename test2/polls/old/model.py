8d2875426c764aeda470506e29c7bbc527e4c69a
mikan3264 <ishii.hiro.80213235@gmail.com>
2019-03-23 12:01:44 +0900

    新規差分

---
+++ test2/polls/old/model.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+# ○○Fieldとなっているものは、Filedインスタンスである
+
+class Question(models.Model):
+   question_text = models.CharField(max_length=200)
+   pub_date = models.DataTimeField('date published') # 人間に可読可能な名前を付けた
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # Choiceが1つのQuestionに対応していると定義している
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
