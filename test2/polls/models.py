05bbc429c665450e3ca00aafc16fbb695380650f
mikan3264 <ishii.hiro.80213235@gmail.com>
2019-03-23 12:12:48 +0900

    スペルミス修正（makemigration実行完了）

--- test2/polls/models.py
+++ test2/polls/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Question(models.Model):
    question_text = models.CharField(max_length=200)
-   pub_date = models.DataTimeField('date published') # 人間に可読可能な名前を付けた
+   pub_date = models.DateTimeField('date published') # 人間に可読可能な名前を付けた
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # Choiceが1つのQuestionに対応していると定義している
