872f7429f599d39efbc5a918b10dfbb2f89ab0b9
mikan3264 <ishii.hiro.80213235@gmail.com>
2019-03-30 23:40:00 +0900

    str関数追加

--- test2/polls/models.py
+++ test2/polls/models.py
@@ -1,13 +1,22 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 # ○○Fieldとなっているものは、Filedインスタンスである
 
 class Question(models.Model):
    question_text = models.CharField(max_length=200)
-   pub_date = models.DateTimeField('date published') # 人間に可読可能な名前を付けた
+   pub_date = models.DateTimeField('date published') # これで人間に可読可能な名前を付けた
+   def __str__(self):
+        return self.question_text
+   def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # Choiceが1つのQuestionに対応していると定義している
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
